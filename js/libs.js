467bac4eb58c7c8547f2caad163f54f2471a3c4f
Framework25 <zinset@ukr.net>
2018-06-13 13:58:29 +0300

    fix

---
+++ js/libs.js
@@ -0,0 +1,10 @@
+/*if ($(window).width() > 991) {
+    var stickySidebar = new StickySidebar('.men-bg', {
+        topSpacing: 20,
+        bottomSpacing: 20,
+        containerSelector: 'footer',
+        innerWrapperSelector: '.men-bg'
+    });
+} else {
+    $('.men-bg').addClass('sticky');
+}*/
